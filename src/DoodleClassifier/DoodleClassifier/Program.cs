4958d7ed6d4b4c0664ae365ec161ce4c5ae37765
vladimir-siv <vladimir.sivcev@gmail.com>
2020-10-20 22:43:13 +0200

    Project Setup

---
+++ src/DoodleClassifier/DoodleClassifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DoodleClassifier
+{
+	public static class Program
+	{
+		[STAThread] private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new PreviewForm());
+		}
+	}
+}
